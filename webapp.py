bb97d3761059d1f238872c31137b2354fd002c6b
Sourav <souav10mohanty@gmail.com>
2020-11-18 11:21:54 +0530

    1

--- webapp.py
+++ webapp.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
