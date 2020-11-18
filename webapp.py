7929c861edd68cdf1ab13d4c8bf00b0864150f3c
Sourav Mohanty <sourav10mohanty@gmail.com>
2020-11-18 10:45:20 +0530

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PIL import Image, ImageTk
+
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
@@ -29,10 +29,11 @@ if file_image is None:
 else:
     file_bytes = np.asarray(bytearray(file_image.read()), dtype=np.uint8)
     img = cv2.imdecode(file_bytes, 1)
+    st.success("INPUT IMAGE")
     st.image(img, channels="BGR",use_column_width=True)
     faces = classifier.detectMultiScale(cv2.cvtColor(img,cv2.COLOR_BGR2GRAY),1.1,5,minSize=(60, 60))
     for face in faces:
-        
+
         slicedImg = img[face[1]:face[1]+face[3],face[0]:face[0]+face[2]]
         slicedImg = cv2.cvtColor(slicedImg, cv2.COLOR_BGR2RGB)
         slicedImg = cv2.resize(slicedImg, (224, 224))
@@ -43,8 +44,9 @@ else:
         acc = np.max(pred*100)
         s = str(acc)
         pred = np.argmax(pred)
-        
+
         cv2.rectangle(img,(face[0],face[1]),(face[0]+face[2],face[1]+face[3]),colorMap[pred],2)
         cv2.putText(img, resMap[pred]+s+"%",(face[0],face[1]-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
-    st.write("**Output Pencil Sketch**")
+    st.success("OUTPUT IMAGE")
     st.image(img, channels="BGR", use_column_width=True)
+    st.success("Detection Accuracy : "+s+"%")
